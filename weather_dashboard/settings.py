591f10ce3a184c3cbd3555ced9aa5e1c6ebaabef
EpatusDataNinja <lebbie1.titus14@gmail.com>
2025-03-19 15:11:15 +0200

    Change static files storage backend

--- weather_dashboard/settings.py
+++ weather_dashboard/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 ]
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
